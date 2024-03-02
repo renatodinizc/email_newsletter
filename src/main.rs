2b9444415718bceedfd7e9cdcd5bc5ce7646baa2
Renato Diniz <renatodiniz25@gmail.com>
2024-03-02 14:32:28 -0300

    tests: add spec for health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
