93a1ac0e39ac03006ddad99c8ae824fbd4f56661
Renato Diniz <renatodiniz25@gmail.com>
2024-03-03 06:50:30 -0300

    feat: receive form data in /subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use email_newsletter::run;
+use email_newsletter::configuration::get_configuration;
+use email_newsletter::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = std::net::TcpListener::bind(address)?;
     run(listener)?.await
 }
